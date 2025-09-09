6a2564d5a486f8bd5f8f9c7e776cf84ff48e55f9
lorentkrz <60048412+lorentkrz@users.noreply.github.com>
2025-09-09 10:06:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
+    /* // test
      * Match all request paths except:
      * - _next/static (static files)
      * - _next/image (image optimization files)
